0e9e790008f7e21f1bb4f4fb630f7f04d75d6096
stushurik <stu.shurik@gmail.com>
2013-10-26 23:08:29 +0300

    sftp

---
+++ dispatch_service/utils/sftp.py
@@ -0,0 +1,71 @@
+import os
+import pysftp
+
+
+class SFTP():
+    def __init__(self):
+
+        self.valid = True
+
+        if not (
+            hasattr(self, 'host') or
+            hasattr(self, 'port') or
+            hasattr(self, 'username') or
+            hasattr(self, 'password')
+        ):
+            dir_name = os.path.dirname(__file__)
+            project_dir = os.path.realpath(os.path.join(dir_name, '..'))
+
+            with open(project_dir+'/conf.ini', 'r') as conf_file:
+                values = []
+                for line in conf_file.readlines():
+                    if str(line).startswith('#'):
+                        continue
+                    value = str(line).split('=')
+
+                    if len(value) < 2:
+                        continue
+                    else:
+                        param = value[0].strip()
+                        setattr(self, param, value[1].strip())
+                        values.append(param)
+
+            print values, type(values)
+            for opt in ('host', 'port', 'username', 'password'):
+                print opt
+                if opt not in values:
+                    print 'its a trap!'
+                    self.valid = False
+
+    def __enter__(self):
+        return self
+
+    def connect(self):
+        print self.valid
+
+        if self.valid:
+            connection = pysftp.Connection(
+                host=self.host,
+                port=int(self.port),
+                username=self.username,
+                password=self.password,
+            )
+
+            setattr(self, 'connection', connection)
+
+    def put_file_to_remote_host(self, file_name):
+        if hasattr(self, 'connection'):
+            self.connection.put(file_name)
+
+    def get_file_from_remote_host(self, file_name):
+        if hasattr(self, 'connection'):
+            self.connection.get(file_name)
+
+    def __exit__(self, type, value, traceback):
+        if hasattr(self, 'connection'):
+            self.connection.close()
+
+if __name__ == "__main__":
+    with SFTP() as sftp:
+        sftp.connect()
+        sftp.put_file_to_remote_host('__init__.py')
