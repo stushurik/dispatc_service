015fed7c3c879eadfd231f29e7ff2234ae8bc4fa
stushurik <stu.shurik@gmail.com>
2013-11-13 12:45:07 +0200

    add migrations

--- dispatch_service/decisions/tests.py
+++ dispatch_service/decisions/tests.py
@@ -4,8 +4,13 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+import datetime
+from django.contrib.auth.models import User
 
 from django.test import TestCase
+
+from decisions.models import Decision
+from events.models import Event
 
 
 class SimpleTest(TestCase):
@@ -14,3 +19,34 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+class ModelsTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='test', password='test')
+        self.event = Event.objects.create(
+            author=self.user,
+            executor=self.user,
+            description='description',
+        )
+
+    def test_create_decision(self):
+        d = Decision.objects.create(
+            author=self.user,
+            description='description',
+            deadline=datetime.datetime.today(),
+            event=self.event
+        )
+
+        self.assertTrue(Decision.objects.filter(
+            author=self.user,
+            description='description',
+            deadline=datetime.datetime.today(),
+            event=self.event
+        ).exists())
+
+        self.assertEquals(d.author.id, self.user.id)
+        self.assertEquals(d.description, 'description')
+        self.assertEquals(d.deadline.day, datetime.datetime.today().day)
+        self.assertEquals(d.event.id, self.event.id)
+
--- dispatch_service/events/tests.py
+++ dispatch_service/events/tests.py
@@ -4,8 +4,13 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+import datetime
+from django.contrib.auth.models import User
 
 from django.test import TestCase
+
+from decisions.models import Decision
+from events.models import Event
 
 
 class SimpleTest(TestCase):
@@ -14,3 +19,42 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+class ModelsTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='test', password='test')
+        self.event = Event.objects.create(
+            author=self.user,
+            executor=self.user,
+            description='description2',
+        )
+        self.dec = Decision.objects.filter(
+            author=self.user,
+            description='description',
+            deadline=datetime.datetime.today(),
+            event=self.event
+        )
+
+    def test_create_decision(self):
+        e = Event.objects.create(
+            author=self.user,
+            executor=self.user,
+            description='description',
+        )
+
+        self.assertTrue(Event.objects.filter(
+            author=self.user,
+            description='description2',
+        ).exists())
+
+        self.assertTrue(Event.objects.filter(
+            author=self.user,
+            description='description',
+        ).exists())
+
+        self.assertEquals(e.author.id, self.user.id)
+        self.assertEquals(e.executor.id, self.user.id)
+        self.assertEquals(e.description, 'description')
+
+
